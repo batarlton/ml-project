de9c6241ba95755bbf7a0d9babd5f464e2e3fb3f
Sean Farris <sean_farris@sbcglobal.net>
2016-08-06 16:22:00 -0500

    Add files via upload

--- S_Project.R
+++ S_Project.R
@@ -2,7 +2,7 @@ library(twitteR)
 library(tm)
 library(qdap)
 library(quantmod)
-
+library(rminer)
 # Word lists are modified versions from Hu and Liu
 #    Minqing Hu and Bing Liu. "Mining and Summarizing Customer Reviews." 
 #        Proceedings of the ACM SIGKDD International Conference on Knowledge 
@@ -14,19 +14,27 @@ Jan <- readLines("January2015.txt")
 Febr <- readLines("February2015.txt")
 March <- readLines("March2015.txt")
 
+commonWords <- paste0(findFreqTerms(as.DocumentTermMatrix(Jan), lowfreq = 2))
 
+# 
 
-getSymbols("AAPL", from = '2015-01-01', to = '2015-01-31')
-AAPLChange <- AAPL$AAPL.Close[[nrow(AAPL$AAPL.Close)]]-AAPL$AAPL.Open[[1]]
-
+# getSymbols("AAPL", from = '2015-01-01', to = '2015-01-31')
+# AAPLChange <- AAPL$AAPL.Close[[nrow(AAPL$AAPL.Close)]]-AAPL$AAPL.Open[[1]]
+# 
 blist = list()
 
 hlist <- list()
 for (h in 1:length(Jan)){
   pos <- sum(str_count(Jan[h], positives))
   neg <- sum(str_count(Jan[h], negatives))
-  hlist[[h]]<-c(pos,neg)
+  t2<-paste0(c(str_extract(Jan[h], commonWords), sign(pos-neg+0.5)))
+  hlist[[h]]<-t2
 }
+t3<-as.data.frame(matrix(unlist(hlist),ncol = length(hlist[[1]]), byrow = T),stringAsFactors =TRUE)
+colnames(t3)<-c(commonWords, "class")
+model <- fit(class~., data = t3[1:500,], model = "naiveBayes", task="class")
+p <- predict(model, t3[501:1000,])
+
 blist[[1]] <- c(Reduce("+",hlist), AAPLChange)
 
 getSymbols("AAPL", from = '2016-07-29', to = '2016-08-06')
@@ -51,5 +59,7 @@ for (i in 1:nrow(AAPLChange)){
   }
   alist[[i]] <- c(Reduce("+",tlist), AAPLChange[i,1])
 }
+
+
 
 print(alist)

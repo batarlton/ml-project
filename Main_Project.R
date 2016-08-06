143cec8ad4699ed8a96df10734fc65af18addaa2
Micah Leigh <leighsoftware@gmail.com>
2016-08-06 15:51:53 -0500

    Attempt at using ML

--- Main_Project.R
+++ Main_Project.R
@@ -1,74 +1,95 @@
 #References
 # https://cran.r-project.org/web/packages/twitteR/twitteR.pdf
 # https://www.credera.com/blog/business-intelligence/twitter-analytics-using-r-part-2-create-word-cloud/
+#credera was used mostly for pulling the data from twitter and parsing it to get rid of unwanted symbols/spaces/weblinks/etc
 
 
-
-#install.packages("twitteR", dependencies = TRUE)
-#install.packages("ROAuth", dependencies = TRUE)
-#install.packages("tm", dependencies = TRUE)
-#install.packages("wordcloud", dependencies = TRUE)
 #install.packages("syuzhet", dependencies = TRUE)
+#install.packages("plyr", dependencies = TRUE)
+#install.packages("neuralnet", dependencies = TRUE)
+#install.packages("e1071", dependencies = TRUE)
 
-library("twitteR")
-library("ROAuth")
-library("tm")
-library("wordcloud")
+
 library("syuzhet")
+library("plyr")
+library("neuralnet")
+library("e1071")
 
-setup_twitter_oauth("zxYRrt8ehfhkzObPHaS0ogvPH", "2NlLd6164qb5Dr7594HqTp8jGthfrRY0A4bU0uJAXcrQe8zbQu", "752704258428968960-ekoMIp6sj0Sg2AQL6zGJObVWCxqzHKy", "8fBFRwtatrp3a4axwOLYDoITI3kFjX7TbzpMXKWLTxtfq")
+# Due to the nature of the API we were forced to change the program so we could pull more tweets and save them. 
+#  There was approx 480 api command limit and a decent amount of tweets but it had a limit too, since we were 
+#  comparing a year at a time, it would have pushed us over the limit. Hence, we pulled about 100 a day for 
+#  every day of 2015 and saved the tweets in files according to months
+fileNames <- c("January2015.txt", "February2015.txt", "March2015.txt", "April2015.txt", "May2015.txt", "June2015.txt", "July2015.txt", "August2015.txt", "September2015.txt", "October2015.txt", "November2015.txt", "December2015.txt")
 
-
-# search will start at the "until", and end at "since"
-#tweetsAAPL <- searchTwitter("AAPL", n=10, lang = "en", since='2016-07-02', until='2016-08-02')
-#tweetsAAPL <- strip_retweets(tweetsAAPL)
-
-numOfTweets <- 10 
-
-# search will start at the "until", and end at "since"
-for(i in 1:28)
+for(i in 1:length(fileNames))
 {
-  date <- paste("2016-07-", i, sep = "")
-  print(date)
-  tweetsAAPL <- sapply(searchTwitter("AAPL", n=numOfTweets, lang = "en", since=date), function(x) x$getText())
-  print(tweetsAAPL)
+  filePath <- getwd()
+  currentFile <- sprintf("%s/%s", filePath, fileNames[i])
+  temp <- read.csv(currentFile, header = FALSE, sep = "\n")
   if(i == 1)
-    monthMatrix <- as.matrix(tweetsAAPL.text)
+  {
+    tweetMatrix <- as.matrix(temp)
+  }
   else
-    monthMatrix <- cbind(monthMatrix, c(tweetsAAPL.text))
+  {
+    tweetMatrix <- cbind(tweetMatrix, temp)
+  }
 }
-
-#https://www.credera.com/blog/business-intelligence/twitter-analytics-using-r-part-2-create-word-cloud/
-#  Above website showed how to obtain tweets and remove unnecessary words/symbols
-# Replace blank space ("rt")
-tweetsAAPL.text <- gsub("rt", "", tweetsAAPL.text)
-# Replace @UserName
-tweetsAAPL.text <- gsub("@//w+", "", tweetsAAPL.text)
-# Remove punctuation
-tweetsAAPL.text <- gsub("[[:punct:]]", "", tweetsAAPL.text)
-# Remove links
-tweetsAAPL.text <- gsub("http\\w+", "", tweetsAAPL.text)
-#convert all text to lower case
-tweetsAAPL.text <- tolower(tweetsAAPL.text)
-# Remove tabs
-tweetsAAPL.text <- gsub("[ |]t]{2,}", "", tweetsAAPL.text)
-# Remove blank spaces at the beginning
-tweetsAAPL.text <- gsub("^ ", "", tweetsAAPL.text)
-# Remove blank spaces at the end
-tweetsAAPL.text <- gsub(" $", "", tweetsAAPL.text)
-
-#TODO: find how to remove retweets
-
-#create word cloud
-#create corpus
-tweetsAAPL.text.corpus <- Corpus(VectorSource(tweetsAAPL.text))
-#clean up by removing stop words
-tweetsAAPL.text.corpus <- tm_map(tweetsAAPL.text.corpus, function(x)removeWords(x, stopwords()))
-#wordcloud(tweetsAAPL.text.corpus,min.freq = 2, scale=c(7,0.5),colors=brewer.pal(8, "Dark2"),  random.color= TRUE, random.order = FALSE, max.words = 150)
-
-
+colnames(tweetMatrix) <- gsub(".txt", "", fileNames)
 
 # get sentiment of the tweets
-tweetsAAPL.text.sentiment <- get_sentiment(tweetsAAPL.text, method = "syuzhet")
+for(i in 1:ncol(tweetMatrix))
+{
+  tweetString <- as.character(tweetMatrix[[i]])
+  sentiment <- get_sentiment(tweetString)
+  theSum <- sum(sentiment)
+  total <- length(sentiment)
+  average <- theSum/total
+  sentiment[sentiment != 0]
+  totalPos <- length(positiveIndexes)
+  totalNeg <- length(negativeIndexes)
+  if(i == 1)
+  {
+    tweetSentimentTotal <- theSum
+    tweetSentimentAverage <- average
+  }
+  else
+  {
+    tweetSentimentTotal <- cbind(tweetSentimentTotal, theSum)
+    tweetSentimentAverage <- cbind(tweetSentimentAverage, average)
+  }
+}
+
+
+
+#needs to be run after stock analysis.R
+tweetVSstock <- cbind(t(tweetSentimentAverage), c(t(changeMatrix)))
+rownames(tweetVSstock) <- gsub(".txt", "", fileNames)
+colnames(tweetVSstock) <- c("Sentiment", "Class")
+
+
+
+yearSentimentAvg <- sum(tweetSentimentAverage)/length(tweetSentimentAverage)
+yearStockAvg <- sum(changeMatrix)/length(changeMatrix)
+
+tweetVSstock<-as.matrix(tweetVSstock)
+
+for(i in 1:nrow(tweetVSstock))
+{
+  if(tweetVSstock[i,1] < yearSentimentAvg)
+    tweetVSstock[i,1] <- 0
+  else
+    tweetVSstock[i,1] <- 1
+  
+  if(tweetVSstock[i,2] < yearStockAvg)
+    tweetVSstock[i,2] <- 0
+  else
+    tweetVSstock[i,2] <- 1
+}
+
+nn <- neuralnet(formula = tweetVSstock[,2]~tweetVSstock[,1], data = tweetVSstock, hidden = 5)
+nb <- naiveBayes(formula = tweetVSstock[,2]~tweetVSstock[,1], data = tweetVSstock)
+#not sure this is useful
+km <- kmeans(tweetVSstock, centers= 2, iter.max = 10)
 
 

e4d60af89370a56df86ba24905deac5a606a3360
Sean Farris <sean_farris@sbcglobal.net>
2016-08-06 18:15:23 -0500

    Add files via upload

--- S_Project.R
+++ S_Project.R
@@ -3,6 +3,7 @@ library(tm)
 library(qdap)
 library(quantmod)
 library(rminer)
+
 # Word lists are modified versions from Hu and Liu
 #    Minqing Hu and Bing Liu. "Mining and Summarizing Customer Reviews." 
 #        Proceedings of the ACM SIGKDD International Conference on Knowledge 
@@ -14,13 +15,13 @@ Jan <- readLines("January2015.txt")
 Febr <- readLines("February2015.txt")
 March <- readLines("March2015.txt")
 
+# Gets frequent words in January text
 commonWords <- paste0(findFreqTerms(as.DocumentTermMatrix(Jan), lowfreq = 2))
 
-# 
 
-# getSymbols("AAPL", from = '2015-01-01', to = '2015-01-31')
-# AAPLChange <- AAPL$AAPL.Close[[nrow(AAPL$AAPL.Close)]]-AAPL$AAPL.Open[[1]]
-# 
+getSymbols("AAPL", from = '2015-01-01', to = '2015-01-31')
+AAPLChange <- AAPL$AAPL.Close[[nrow(AAPL$AAPL.Close)]]-AAPL$AAPL.Open[[1]]
+
 blist = list()
 
 hlist <- list()
@@ -32,34 +33,56 @@ for (h in 1:length(Jan)){
 }
 t3<-as.data.frame(matrix(unlist(hlist),ncol = length(hlist[[1]]), byrow = T),stringAsFactors =TRUE)
 colnames(t3)<-c(commonWords, "class")
-model <- fit(class~., data = t3[1:500,], model = "naiveBayes", task="class")
-p <- predict(model, t3[501:1000,])
 
-blist[[1]] <- c(Reduce("+",hlist), AAPLChange)
+###############
+#Test tweet classification
+###############
 
-getSymbols("AAPL", from = '2016-07-29', to = '2016-08-06')
-AAPLChange <- as.data.frame(AAPL$AAPL.Close-AAPL$AAPL.Open)
-AAPLChange$date <- as.Date(rownames(AAPLChange))
+traint3<-sample(nrow(t3),100)
 
-alist = list()
+trainset <- t3[traint3,]
+testset <- t3[-traint3,]
 
-for (i in 1:nrow(AAPLChange)){
-  sdate <-as.character(AAPLChange[i,2]-1)
-  udate <-as.character(AAPLChange[i,2]+1)
-  tweetsAAPL <- searchTwitter("#AAPL", n=50, since = sdate, until = udate)
-  tweetsAAPL <- do.call("rbind", lapply(tweetsAAPL, as.data.frame))[,"text"]
-  tweetsAAPL <- str_to_lower(rm_url(tweetsAAPL))
-  
-  tlist = list()
-  
-  for (j in 1:length(tweetsAAPL)){
-    pos <- sum(str_count(tweetsAAPL[j], positives))
-    neg <- sum(str_count(tweetsAAPL[j], negatives))
-    tlist[[j]]<-c(pos,neg)
-  }
-  alist[[i]] <- c(Reduce("+",tlist), AAPLChange[i,1])
-}
+#Naive Bayes
+model <- fit(class~., data = trainset, model = "naiveBayes", task="class")
+p <- predict(model, testset)
+accp <- mmetric(p, t3[-traint3,"class"], "ACC")
+print(accp)
+
+#SVM
+model <- fit(class~., data = trainset, model = "svm", task="class")
+p <- predict(model, testset)
+accp <- mmetric(p, t3[-traint3,"class"], "ACC")
+print(accp)
 
 
 
-print(alist)
+
+# blist[[1]] <- c(as.numeric(t3[["class"]])-1, AAPLChange)
+
+# getSymbols("AAPL", from = '2016-07-29', to = '2016-08-06')
+# AAPLChange <- as.data.frame(AAPL$AAPL.Close-AAPL$AAPL.Open)
+# AAPLChange$date <- as.Date(rownames(AAPLChange))
+
+# alist = list()
+# 
+# for (i in 1:nrow(AAPLChange)){
+#   sdate <-as.character(AAPLChange[i,2]-1)
+#   udate <-as.character(AAPLChange[i,2]+1)
+#   tweetsAAPL <- searchTwitter("#AAPL", n=50, since = sdate, until = udate)
+#   tweetsAAPL <- do.call("rbind", lapply(tweetsAAPL, as.data.frame))[,"text"]
+#   tweetsAAPL <- str_to_lower(rm_url(tweetsAAPL))
+#   
+#   tlist = list()
+#   
+#   for (j in 1:length(tweetsAAPL)){
+#     pos <- sum(str_count(tweetsAAPL[j], positives))
+#     neg <- sum(str_count(tweetsAAPL[j], negatives))
+#     tlist[[j]]<-c(pos,neg)
+#   }
+#   alist[[i]] <- c(Reduce("+",tlist), AAPLChange[i,1])
+# }
+# 
+# 
+# 
+# print(alist)
